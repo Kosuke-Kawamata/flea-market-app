b95128b8f06c8deb59e733acd617c7fd2e40a9fb
Kosuke-Kawamata <kkbg.10bmg@gmail.com>
2021-06-03 17:11:30 +0900

    awsのアクセスキーを変数に入れて扱う形式に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIASU5QXMTWAPUHDYD2',
-    aws_secret_access_key: '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',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
@@ -20,5 +20,5 @@ CarrierWave.configure do |config|
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test.flea-market-app'
   end
   config.remove_previously_stored_files_after_update = true
-  
+  config.fog_public = false 
 end
